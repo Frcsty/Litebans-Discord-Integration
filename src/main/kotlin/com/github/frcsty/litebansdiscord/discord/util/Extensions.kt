721f47fba0aa8ebaf491b58da9d93704787c8585
Frosty <frostyprime13@gmail.com>
2020-11-27 09:31:49 +0100

    Fixed some minor issues with it's functionality, all working now.
    (Still, use at your own risk as it executes direct quarries and may clog up shit)

--- src/main/kotlin/com/github/frcsty/litebansdiscord/discord/util/Extensions.kt
+++ src/main/kotlin/com/github/frcsty/litebansdiscord/discord/util/Extensions.kt
@@ -1,11 +1,11 @@
 package com.github.frcsty.litebansdiscord.discord.util
 
 import litebans.api.Database
-import net.dv8tion.jda.core.Permission
-import net.dv8tion.jda.core.entities.Member
-import net.dv8tion.jda.core.entities.Message
-import net.dv8tion.jda.core.entities.Role
-import net.dv8tion.jda.core.entities.User
+import net.dv8tion.jda.api.Permission
+import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.entities.Role
+import net.dv8tion.jda.api.entities.User
 import org.bukkit.Bukkit
 import org.bukkit.OfflinePlayer
 import java.sql.SQLException
@@ -43,15 +43,16 @@ fun OfflinePlayer.getUserBans(): List<InformationHolder> {
     try {
         Database.get().prepareStatement(query).use { statement ->
             statement.setString(1, uuid)
+
             statement.executeQuery().use { result ->
                 while (result.next()) {
-                    val reason = result.getString("reason")?: "Unknown"
-                    val bannedByName = result.getString("banned_by_name")?: "Unknown"
+                    val reason = result.getString("reason") ?: "Unknown"
+                    val bannedByName = result.getString("banned_by_name") ?: "Unknown"
                     val time = result.getLong("time")
                     val until = result.getLong("until")
                     val active = result.getBoolean("active")
-                    val serverScope = result.getString("server_scope")?: "Unknown"
-                    val serverOrigin = result.getString("server_origin")?: "Unknown"
+                    val serverScope = result.getString("server_scope") ?: "Unknown"
+                    val serverOrigin = result.getString("server_origin") ?: "Unknown"
                     val silent = result.getBoolean("silent")
                     val ipBan = result.getBoolean("ipban")
                     holders.add(InformationHolder(reason, bannedByName, time, until, active, serverScope, serverOrigin, silent, ipBan))
@@ -71,12 +72,13 @@ fun OfflinePlayer.getUserIPHistory(): List<HistoryHolder> {
     try {
         Database.get().prepareStatement(query).use { statement ->
             statement.setString(1, uuid)
+
             statement.executeQuery().use { result ->
                 while (result.next()) {
                     val id = result.getLong("id")
                     val date = result.getTimestamp("date")
-                    val name = result.getString("name")?: "Unknown"
-                    val uuidString = result.getString("uuid")?: "Unknown"
+                    val name = result.getString("name") ?: "Unknown"
+                    val uuidString = result.getString("uuid") ?: "Unknown"
                     var ip = result.getString("ip")
                     if (ip.equals("#", ignoreCase = false)) ip = "Unknown"
                     holders.add(HistoryHolder(id, date, name, uuidString, ip))
